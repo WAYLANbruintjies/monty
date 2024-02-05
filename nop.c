ec6d0c6a56073efa7f4322a7450684f0f85a327a
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-05 04:20:29 -0800

    nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *f_nop - Doesn't do anything
+  *@head: head of the stack
+  *@counter: line number
+  *Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
