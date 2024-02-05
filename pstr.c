49adb040024fc63aa1ba9487fa7dddbc735c5062
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-02-05 04:12:22 -0800

    pint

---
+++ pstr.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * f_pstr - Prints the value at top of the stack, followed by a new line
+ * @head: head of the stack
+ * @counter: line number
+ * Return: void
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *stk = *head;
+	(void)counter;
+
+	while (stk != NULL) /* while (stk == TRUE) */
+	{
+		if (stk->n > 127 || stk->n <= 0)
+		{
+			break;
+		}
+		stk = stk->next;
+		printf("%c", stk->n);
+	}
+	printf("\n");
+}
